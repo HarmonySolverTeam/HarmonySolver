e73e5b051e4fabbd209f27b2922d180ee9d4be0b
wiktor145 <viktor145@wp.pl>
2020-10-21 20:04:55 +0200

    HS-1 showing broken rules report

---
+++ src/main/objects/BrokenRulesCounter.js
@@ -0,0 +1,28 @@
+function BrokenRulesCounter(rulesList, rulesDetails) {
+    this.rulesList = rulesList
+    this.rulesDetails = rulesDetails
+    this.allConnections = 0
+
+    for (var i = 0; i < this.rulesList.length; i++) {
+        this[this.rulesList[i]] = 0
+    }
+
+    this.increaseCounter = function(ruleName) {
+        this[ruleName] += 1
+    }
+
+    this.setAllConnections = function(value) {
+        this.allConnections = value
+    }
+
+    this.getBrokenRulesStringInfo = function() {
+        var ret = ""
+
+        for (var i = 0; i < this.rulesList.length; i++) {
+            if (this[this.rulesList[i]] !== 0) {
+                ret += this.rulesDetails[i] + ": " + this[this.rulesList[i]] + "/" + this.allConnections + "\n"
+            }
+        }
+        return ret
+    }
+}
